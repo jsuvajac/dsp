a48b34810cff98f8744483b3b8618ca7387808e8
Jovan <suvajacjovan@gmail.com>
2020-06-06 01:55:21 -0400

    slice speed and repeat refactor

--- slice.py
+++ slice.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-import wave
 import simpleaudio as sa # TODO: replace with pyaudio
 import numpy as np
+import wave
 import struct
 
 class Slice:
@@ -27,44 +27,30 @@ class Slice:
             offset=offset,
             dtype=np.int16)
 
-    def write(self, out_name: str, repeats: int = 1, speed: float = 1):
-        if speed == 0:
-            return
-        out = wave.open(out_name, 'wb')
-        out.setnchannels(1)
-        out.setsampwidth(2)
-        out.setframerate(44100.0)
-
+    def repeat(self, repeats: int = 1):
         out_list = []
-
-        # insert samples num_repeat times
         for i in range(repeats):
             for x in range(len(self.samples)):
                 out_list.append(self.samples[x])
+        self.samples = out_list
 
-        # TODO: breakup into methods
+    def update_speed(self, speed: float = 1):
+        out_list = []
+        if speed == 0:
+            return
         # write
         if speed >= 1:
-            for i in range(len(out_list)):
-                if i % speed == 0:
-                    out.writeframesraw(struct.pack("<h", out_list[i]))
+            out_list = [self.samples[i] for i in range(len(self.samples)) if i % speed == 0]
         elif speed <= -1:
-            for i in range(len(out_list)):
-                if i % speed == 0:
-                    out.writeframesraw(struct.pack("<h", out_list[-i]))
+            out_list = [self.samples[-i] for i in range(len(self.samples)) if i % speed == 0]
         # slowdown
         elif speed < 1 and speed > 0:
-            for i in range(len(out_list)): 
-                for x in range(int(1/speed)):
-                    out.writeframesraw(struct.pack("<h", out_list[i]))
+            out_list = [self.samples[i] for i in range(len(self.samples)) for x in range(int(1/speed))]
         elif speed > -1 and speed < 0:
-            for i in range(len(out_list)): 
-                for x in range(-int(1/speed)):
-                    out.writeframesraw(struct.pack("<h", out_list[-i]))
+            out_list = [self.samples[-i] for i in range(len(self.samples)) for x in range(-int(1/speed))]
+            
+        self.samples = out_list
         
-        out.close()
-        return out_list
-
     def write_slice(self, out_name: str, start: int, end: int):
         out = wave.open(out_name, 'wb')
         out.setnchannels(1)
@@ -74,6 +60,7 @@ class Slice:
         for i in range(start, end):
             out.writeframesraw(struct.pack("<h", self.samples[i]))
 
+    #TODO: fix playback after speed or repeat change
     def play(self, start=0, end=-1):
         play_obj = sa.play_buffer(self.samples[start:end], 1, 2, 44100)
         #play_obj.wait_done()
