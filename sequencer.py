0b978115703d9a0a267ee203592cc18d90766c66
Jovan <suvajacjovan@gmail.com>
2020-06-17 14:44:20 -0400

    done euclidean sequencer

--- sequencer.py
+++ sequencer.py
@@ -1,26 +1,30 @@
 import threading
 import time
+from math import gcd
 
 from wav import *
 
 
 class Pattern:
-    def __init__(self, wav, beat_count):
+    def __init__(self, wav, num_steps):
         self.wav = wav
 
-        self.pattern = [1 for f in range(beat_count)]
-        self.beat_count = beat_count
+        self.pattern = [1 for f in range(num_steps)]
+        self.num_steps = num_steps
         # auto set length based on number of hits and sample length
-        self.length = len(self.wav.samples)/44100.0 * beat_count
-        #print(self.pattern, self.length, self.beat_count)
+        self.length = len(self.wav.samples)/44100.0 * num_steps
+        #print(self.pattern, self.length, self.num_steps)
+
     def lengthen(self, factor):
-        print(self.pattern, factor)
+        #print(self.pattern, factor)
+
         temp = []
         for i in self.pattern:
+            temp.append(i)
             for j in range(factor-1):
                 temp.append(0)
-            temp.append(i)
         self.pattern = temp
+        self.num_steps = len(temp)
 
 
 
@@ -37,42 +41,47 @@ class Sequencer:
 
     def add_pattern(self, pattern):
         if self.sequence == []:
-            self.step_size = pattern.length/pattern.beat_count
-            self.num_steps = pattern.beat_count
+            self.step_size = pattern.length/pattern.num_steps
+            self.num_steps = pattern.num_steps
             self.sequence.append(pattern)
 
-        elif self.num_steps == pattern.beat_count:
+        elif self.num_steps == pattern.num_steps:
             self.sequence.append(pattern)
             return
 
         else:
-            self.num_steps *= pattern.beat_count
-            self.step_size = self.step_size/pattern.beat_count
+            gcd_val = gcd(self.num_steps,pattern.num_steps)
 
-            # TODO: fix with gcd
+            #print(self.num_steps, pattern.num_steps)
+            temp_num_steps = int(self.num_steps*pattern.num_steps/ gcd_val)
+
             for pat in self.sequence:
-                pat.lengthen(pattern.beat_count)
-                print(pat.pattern)
-            pattern.lengthen(int(self.num_steps/pattern.beat_count))
-            print(pattern.pattern)
+                pat.lengthen(int(temp_num_steps/self.num_steps))
+
+            pattern.lengthen(int(temp_num_steps/pattern.num_steps))
+
+            self.num_steps = temp_num_steps
             self.sequence.append(pattern)
 
 
 
-    def on_play_pattern(self, sequence):
-        t = threading.Thread(target=self.play_ntimes, args=(self.sequence, ), daemon=True)
+    def on_play_pattern(self):
+        t = threading.Thread(target=self.play, args=(self.sequence, ))
         t.start()
 
     def play(self, seq):
         index = 0
         while self.run:
-            start = time.time()
-            for i, pat in enumerate(seq):
-                if pat.pattern[index] == 1:
-                    pat.wav.play()
-
             if index == 0:
                 print()
+            print(f"{index+1} ",end='', flush=True)
+
+            start = time.time()
+
+            for i, pat in enumerate(seq):
+                if pat.pattern[index] == 1:
+                    # TODO: replace simple audio with pyaudio for better performance
+                    pat.wav.play()
 
             #print(index, end='')
             index += 1
@@ -94,19 +103,22 @@ class Sequencer:
             #print(f" del: {end-start:.4f}")
 
 if __name__=="__main__":
-    pat1 = Pattern(Wav("slice/slice_1.wav"), 2)
-    #pat1.pattern = [0,0,1,0,1]
+    patterns = []
 
-    pat2 = Pattern(Wav("slice/slice_1.wav"), 3)
-
-    #pat3 = Pattern(Wav("slice/slice_3.wav"), 6)
-    #pat3.pattern = [1,0,1,0,1,0]
+    patterns.append(Pattern(Wav("input/high.wav"), 4))
+    patterns.append(Pattern(Wav("input/mid.wav"), 5))
+    patterns.append(Pattern(Wav("input/low.wav"), 3))
 
     seq = Sequencer()
-    seq.add_pattern(pat1)
-    seq.add_pattern(pat2)
-    seq.step_size = 0.25
-    #seq.add_pattern(pat3)
+
+    for pat in patterns:
+        seq.add_pattern(pat)
+        print(f"beat count: {pat.num_steps} {len(pat.pattern)} patterns:")
+        for p in seq.sequence:
+            print(f"\t{p.pattern}")
+
+
+    seq.step_size = 1/seq.num_steps
 
     try:
         seq.play(seq.sequence)
